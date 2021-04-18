45398c71c12ea5145226e55ff924492c0d9d5497
Stas N <stas-nepomn@ya.ru>
2021-04-18 22:44:25 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import helmet from 'fastify-helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter(),
+    );
+    app.enableCors();
+    app.register(helmet);
+    const configService = app.get(ConfigService);
+    const port = configService.get<number>('port');
+    await app.listen(port);
+    console.log(`Backend is running on ${await app.getUrl()}`);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 bootstrap();
